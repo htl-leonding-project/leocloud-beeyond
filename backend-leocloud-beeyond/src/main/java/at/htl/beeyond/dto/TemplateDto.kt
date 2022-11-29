7e0265b216d11d88f58712b31f2b69a90aa175b1
markusremplbauer <markus.remplbauer@gmx.at>
2022-11-29 16:25:50 +0100

    add Template dto

---
+++ backend-leocloud-beeyond/src/main/java/at/htl/beeyond/dto/TemplateDto.kt
@@ -0,0 +1,32 @@
+package at.htl.beeyond.dto
+
+import at.htl.beeyond.entity.Template
+import java.util.*
+import javax.json.bind.annotation.JsonbTransient
+import javax.validation.Valid
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
+
+class TemplateDto(
+    @set:JsonbTransient var id: Long? = null,
+    @field:NotNull @field:Size(min = 1, max = 255) var name: String? = null,
+    @field:Size(max = 255) var description: String? = null,
+    @field:NotBlank var content: String? = null,
+    @field:Valid var fields: List<TemplateFieldDto> = LinkedList(),
+    @set:JsonbTransient var deleted: Boolean? = null
+) {
+    constructor(template: Template) : this(
+        template.id,
+        template.name,
+        template.description,
+        template.content,
+        template.fields.map { TemplateFieldDto(it) }.toList(),
+        template.deleted
+    )
+
+    override fun toString(): String {
+        return ""
+    }
+}
+
