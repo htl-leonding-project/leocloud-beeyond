f1310d915fca56ae51036022c1fb9636fd01d78c
NicoHirsch1 <nico.hirsch.2004@gmail.com>
2022-11-29 16:38:29 +0100

    add arrow left and right buttons

--- frontend-leocloud-beeyond/pages/index.tsx
+++ frontend-leocloud-beeyond/pages/index.tsx
@@ -1,4 +1,5 @@
 import ListItem from "../components/ListItem";
+import Image from "next/image";
 
 export default function Home() {
     return (
@@ -8,7 +9,16 @@ export default function Home() {
                 <ListItem name={"Test2"}/>
                 <ListItem name={"Test3"}/>
             </div>
-            <div className={"w-1/5"}></div>
+            <div className={"w-1/5 flex items-center justify-center h-full"}>
+                <div className={"flex-row"}>
+                    <div className={"cursor-pointer"}>
+                        <Image src={"/assets/arrow-left.svg"} alt={"arrow-left"} width={48} height={48}/>
+                    </div>
+                    <div className={"cursor-pointer"}>
+                        <Image src={"/assets/arrow-right.svg"} alt={"arrow-right"} width={48} height={48}/>
+                    </div>
+                </div>
+            </div>
             <div className={"w-2/5"}></div>
         </div>
     )
