e3ff0c071548b27fdce7e29d3bac0acf674cd0d4
NicoHirsch1 <nico.hirsch.2004@gmail.com>
2022-11-30 11:00:10 +0100

    add download button

--- frontend-leocloud-beeyond/pages/index.tsx
+++ frontend-leocloud-beeyond/pages/index.tsx
@@ -9,14 +9,18 @@ export default function Home() {
                 <ListItem name={"Test2"}/>
                 <ListItem name={"Test3"}/>
             </div>
-            <div className={"w-1/5 flex items-center justify-center h-full"}>
-                <div className={"flex-row"}>
+            <div className={"w-1/5 flex-col h-full"}>
+                <div className={"h-1/3"}></div>
+                <div className={"h-1/3 flex flex-col justify-center items-center"}>
                     <div className={"cursor-pointer"}>
                         <Image src={"/assets/arrow-left.svg"} alt={"arrow-left"} width={48} height={48} priority={true}/>
                     </div>
                     <div className={"cursor-pointer"}>
                         <Image src={"/assets/arrow-right.svg"} alt={"arrow-right"} width={48} height={48}/>
                     </div>
+                </div>
+                <div className={"h-1/3 flex justify-center items-end"}>
+                    <button className={"font-medium text-white bg-primary rounded-lg shadow-md hover:bg-secondary hover:text-black p-4"}>DOWNLOAD</button>
                 </div>
             </div>
             <div className={"w-2/5 bg-white shadow-md rounded-lg overflow-auto"}>
