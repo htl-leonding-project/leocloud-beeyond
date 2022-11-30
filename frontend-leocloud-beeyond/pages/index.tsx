61e5bb0b4fae0f77c626c855f3595f292e0041bb
NicoHirsch1 <nico.hirsch.2004@gmail.com>
2022-11-30 12:41:43 +0100

    fetch templates from backend

--- frontend-leocloud-beeyond/pages/index.tsx
+++ frontend-leocloud-beeyond/pages/index.tsx
@@ -1,13 +1,20 @@
 import ListItem from "../components/ListItem";
 import Image from "next/image";
+import useSWR from 'swr'
+import { Template } from "../models/template";
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+const url = `${process.env.API_URL}/template`;
 
 export default function Home() {
+    const { data } = useSWR<Template[]>(url, fetcher);
+
     return (
         <div className={"flex h-screen p-8"}>
             <div className={"w-2/5 bg-white shadow-md rounded-lg overflow-auto"}>
-                <ListItem name={"Test1"}/>
-                <ListItem name={"Test2"}/>
-                <ListItem name={"Test3"}/>
+                {
+                    data?.map(template => <ListItem key={template.id} name={template.name}/>)
+                }
             </div>
             <div className={"w-1/5 flex-col h-full"}>
                 <div className={"h-1/3"}></div>
