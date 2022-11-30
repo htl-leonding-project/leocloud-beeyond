fa059e190f323611a49f17d0ce2631de22f64565
NicoHirsch1 <nico.hirsch.2004@gmail.com>
2022-11-30 13:38:53 +0100

    format code

--- frontend-leocloud-beeyond/components/ListItem.tsx
+++ frontend-leocloud-beeyond/components/ListItem.tsx
@@ -1,13 +1,22 @@
 import { Template } from "../models/template";
 
-export default function ListItem({ template, selectedTemplate, setSelectedTemplate }: {
-    template: Template;
-    selectedTemplate: Template;
-    setSelectedTemplate: any;
+export default function ListItem({
+  template,
+  selectedTemplate,
+  setSelectedTemplate,
+}: {
+  template: Template;
+  selectedTemplate: Template;
+  setSelectedTemplate: any;
 }) {
-    return (
-        <div className={`rounded-lg cursor-pointer hover:bg-secondary p-4 ${selectedTemplate?.id == template.id ? "bg-secondary" : ""}`} onClick={() => setSelectedTemplate(template)}>
-            <div className={"font-medium"}>{template.name}</div>
-        </div>
-    )
+  return (
+    <div
+      className={`rounded-lg cursor-pointer hover:bg-secondary p-4 ${
+        selectedTemplate?.id == template.id ? "bg-secondary" : ""
+      }`}
+      onClick={() => setSelectedTemplate(template)}
+    >
+      <div className={"font-medium"}>{template.name}</div>
+    </div>
+  );
 }
