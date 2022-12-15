65bf337845341d24b4f1f8ca70d41b139b675ab7
NicoHirsch1 <nico.hirsch.2004@gmail.com>
2022-12-15 12:04:26 +0100

    implement logic to edit wildcard fields

--- frontend-leocloud-beeyond/components/WildcardForm.tsx
+++ frontend-leocloud-beeyond/components/WildcardForm.tsx
@@ -1,14 +1,33 @@
-import { WildcardField } from "../models/template";
+import { Template, WildcardField } from "../models/template";
 import useStateStore from "../store/stateStore";
+import { useState } from "react";
 
-function FormElement({ label }: { label: string }) {
+function FormElement({
+  wildcard,
+  selectedTemplate,
+}: {
+  wildcard: WildcardField;
+  selectedTemplate: Template;
+}) {
+  const [value, setValue] = useState(wildcard.value);
+  const setActiveTemplate = useStateStore((state) => state.setActiveTemplate);
+
   return (
     <div className="mb-2 mx-2">
       <label className="block text-sm font-semibold text-gray-800">
-        {label}
+        {wildcard.label}
       </label>
       <input
         type="text"
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+          const field = selectedTemplate.fields.find(
+            (w) => w.id === wildcard.id
+          );
+          field!.value = e.target.value;
+          setActiveTemplate(selectedTemplate);
+        }}
         className="block w-full px-4 py-2 mt-2 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40"
       />
     </div>
@@ -23,9 +42,15 @@ export function WildCardForm({}: {}) {
       <hr />
       <form className="mt-6">
         <div>
-          {selectedTemplate?.fields.map((field: WildcardField) => (
-            <FormElement key={field.label} label={field.label}></FormElement>
-          ))}
+          {selectedTemplate?.fields.map(
+            (field: WildcardField, index: number) => (
+              <FormElement
+                key={index}
+                wildcard={field}
+                selectedTemplate={selectedTemplate}
+              ></FormElement>
+            )
+          )}
         </div>
       </form>
     </div>
