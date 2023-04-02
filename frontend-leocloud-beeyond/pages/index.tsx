e43466f76fa4b72a9f605bf19534b3634c862090
markusremplbauer <markus.remplbauer@gmx.at>
2023-04-02 20:15:20 +0200

    leocloud-24 remove unused import

--- frontend-leocloud-beeyond/pages/index.tsx
+++ frontend-leocloud-beeyond/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 import ArrowButton from "@components/ArrowButton";
 import { ErrorAlert } from "@components/ErrorAlert";
