0b7920db8339a07a2f9344f75534a323ae5ad927
markusremplbauer <markus.remplbauer@gmx.at>
2023-04-05 14:19:15 +0200

    leocloud-31 add favicon

--- frontend-leocloud-beeyond/components/layout/Layout.tsx
+++ frontend-leocloud-beeyond/components/layout/Layout.tsx
@@ -2,13 +2,16 @@ import React, { ReactNode } from "react";
 
 import Head from "next/head";
 import { Navbar } from "@components/layout/Navbar";
+import { useEnvContext } from "@stores/envContext";
 
 const Layout = ({ children }: { children: ReactNode }) => {
+  const { basePath } = useEnvContext();
+
   return (
     <>
       <Head>
         <title>Beeyond</title>
-        <link rel="icon" href={`${process.env.BASE_PATH}/favicon.ico`} />
+        <link rel="icon" href={`${basePath}/favicon.ico`} />
       </Head>
       <main className="mx-2 flex h-screen flex-col">
         <Navbar></Navbar>
