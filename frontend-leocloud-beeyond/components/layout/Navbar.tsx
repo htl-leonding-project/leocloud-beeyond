45cb5c23cbed79684336da185ec702ba64c729bb
NicoHirsch1 <nico.hirsch.2004@gmail.com>
2023-04-06 15:07:08 +0200

    leocloud-32 fix navbar alignment with cards

--- frontend-leocloud-beeyond/components/layout/Navbar.tsx
+++ frontend-leocloud-beeyond/components/layout/Navbar.tsx
@@ -4,15 +4,17 @@ export function Navbar() {
   const { basePath } = useEnvContext();
 
   return (
-    <div className="navbar h-12 rounded-lg bg-secondary">
-      <img
-        src={`${basePath}/assets/bee-purple.svg`}
-        className="mr-3 h-6 select-none sm:h-9"
-        alt="Beeyond Logo"
-      />
-      <span className="select-none text-2xl font-semibold text-white">
-        Beeyond
-      </span>
-    </div>
+    <nav className={"p-2"}>
+      <div className="navbar h-12 rounded-lg bg-secondary px-4">
+        <img
+          src={`${basePath}/assets/bee-purple.svg`}
+          className="mr-3 h-6 select-none sm:h-9"
+          alt="Beeyond Logo"
+        />
+        <span className="select-none text-2xl font-semibold text-white">
+          Beeyond
+        </span>
+      </div>
+    </nav>
   );
 }
