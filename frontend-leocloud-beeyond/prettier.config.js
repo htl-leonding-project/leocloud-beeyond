16a9ff1f7fd30a78d0f6af4c8f056ddf1ffb01e9
markusremplbauer <markus.remplbauer@gmx.at>
2023-04-08 14:51:34 +0200

    update prettier config

--- frontend-leocloud-beeyond/prettier.config.js
+++ frontend-leocloud-beeyond/prettier.config.js
@@ -1,10 +1,12 @@
 /** @type {import("prettier").Config} */
 const config = {
-    plugins: [require.resolve("prettier-plugin-tailwindcss")],
-    trailingComma: "es5",
-    semi: true,
-    singleQuote: false,
-    printWidth: 100,
+  plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  trailingComma: "es5",
+  semi: true,
+  singleQuote: false,
+  printWidth: 100,
+  endOfLine: "lf",
+  embeddedLanguageFormatting: "auto",
 };
 
 module.exports = config;
