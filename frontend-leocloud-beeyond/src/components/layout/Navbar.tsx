c1851f20ea43ad9c324bc19d354bad8406a84c05
markusremplbauer <markus.remplbauer@gmx.at>
2023-04-10 12:01:22 +0200

    leocloud-34 remove warnings

--- frontend-leocloud-beeyond/src/components/layout/Navbar.tsx
+++ frontend-leocloud-beeyond/src/components/layout/Navbar.tsx
@@ -39,13 +39,13 @@ export default function Navbar() {
     <div className="navbar h-12 rounded-lg bg-secondary px-4">
       <Image
         src={`${basePath}/assets/bee-purple.svg`}
-        className="mr-3 h-10 select-none"
+        className="mr-3 h-10 select-none w-10"
         alt="Beeyond Logo"
         width={48}
         height={48}
       />
       <span className="select-none text-2xl font-semibold text-base-100">Beeyond</span>
-      <select className="select ml-auto" data-choose-theme>
+      <select data-choose-theme className="select ml-auto">
         <option value="">Default</option>
         {themes.map((theme) => (
           <option key={theme} value={theme}>
