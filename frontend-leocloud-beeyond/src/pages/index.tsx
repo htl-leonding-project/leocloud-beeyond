87ff46de4b62da825916218eb5830d66adbfc12d
markusremplbauer <markus.remplbauer@gmx.at>
2023-04-15 15:23:08 +0200

    leocloud-39 update alert animation  and add animation to WildCardForm

--- frontend-leocloud-beeyond/src/pages/index.tsx
+++ frontend-leocloud-beeyond/src/pages/index.tsx
@@ -104,7 +104,7 @@ export default function Home() {
         if (result) {
           toast = result;
         } else {
-          downloadDeployment();
+          // downloadDeployment();
         }
     }
 
@@ -147,7 +147,13 @@ export default function Home() {
           {
             <AnimatePresence>
               {alert.show && (
-                <motion.div exit={{ opacity: 0 }} className="toast">
+                <motion.div
+                  className="toast"
+                  initial={{ opacity: 0, x: 100 }}
+                  animate={{ opacity: 1, x: 0 }}
+                  exit={{ opacity: 0, x: 100 }}
+                  transition={{ ease: "easeInOut", duration: 0.25 }}
+                >
                   {alert.show && <Alert type={alert.type} message={alert.message} />}
                 </motion.div>
               )}
@@ -158,11 +164,19 @@ export default function Home() {
 
       <div className="flex h-full w-2/5 flex-col space-y-2">
         <TemplateList header="Selected Templates" templates={selectedTemplates} />
-        {selectedTemplates.includes(activeTemplate!) && (
-          <div className="flex h-1/2 w-full overflow-y-auto rounded-lg">
-            <WildCardForm />
-          </div>
-        )}
+        <AnimatePresence>
+          {selectedTemplates.includes(activeTemplate!) && (
+            <motion.div
+              className="flex h-full w-full overflow-y-auto rounded-lg"
+              initial={{ opacity: 0, y: 50 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: 50 }}
+              transition={{ ease: "easeInOut", duration: 0.25 }}
+            >
+              <WildCardForm />
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
     </>
   );
